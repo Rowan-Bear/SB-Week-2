c7b08f35d05d8b0f2f9f38e13fb81ea3176c459f
Rowan Bear <rowan@rowans-computer.lan>
2022-02-14 12:33:36 -0700

    second commit

--- jeep-sales/src/main/java/com/promineotech/jeep/entity/Jeep.java
+++ jeep-sales/src/main/java/com/promineotech/jeep/entity/Jeep.java
@@ -4,7 +4,11 @@ import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Data
 @Builder
@@ -24,53 +28,7 @@ public class Jeep {
   
   private BigDecimal basePrice;
 
-  public Long getModelPK() {
-    return modelPK;
-  }
-
-  public void setModelPK(Long modelPK) {
-    this.modelPK = modelPK;
-  }
-
-  public String getTrimLevel() {
-    return trimLevel;
-  }
-
-  public void setTrimLevel(String trimLevel) {
-    this.trimLevel = trimLevel;
-  }
-
-  public int getNumDoors() {
-    return numDoors;
-  }
-
-  public void setNumDoors(int numDoors) {
-    this.numDoors = numDoors;
-  }
-
-  public JeepModel getModelId() {
-    return modelId;
-  }
-
-  public void setModelId(JeepModel modelId) {
-    this.modelId = modelId;
-  }
-
-  public int getWheelSize() {
-    return wheelSize;
-  }
-
-  public void setWheelSize(int wheelSize) {
-    this.wheelSize = wheelSize;
-  }
-
-  public BigDecimal getBasePrice() {
-    return basePrice;
-  }
-
-  public void setBasePrice(BigDecimal basePrice) {
-    this.basePrice = basePrice;
-  }
+  
   
 }
 
--- jeep-sales/src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
+++ jeep-sales/src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -2,7 +2,10 @@ package com.promineotech.jeep.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+import java.util.LinkedList;
 import java.util.List;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,9 +16,11 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.jdbc.JdbcTestUtils;
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
 import lombok.Getter;
@@ -25,13 +30,12 @@ import lombok.Getter;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 @Sql(scripts = {
-    "classpath:flyway/migrations/V1.0__Jeep_Schema.sql",
-    "classpath:flyway/migrations/V1.1__Jeep_Data.sql"}, 
+    "classpath:flyway/migrations/v1.0_Jeep_Schema.sql",
+    "classpath:flyway/migrations/v1.1_Jeep_Data.sql"}, 
     config = @SqlConfig(encoding = "utf-8"))
 
 class FetchJeepTest {
   @Autowired
-  @Getter
   private TestRestTemplate restTemplate;
   
   @LocalServerPort
@@ -46,10 +50,21 @@ private String getBaseUri() {
   
   return String.format("http://localhost:%d/jeeps", serverPort);
 }
+//Tests in-memory database
+
+
+//@Autowired
+//private JdbcTemplate jdbcTemplate;
+//
+//@Test
+//void testDb() {
+//  int numrows = JdbcTestUtils.countRowsInTable(jdbcTemplate, "customers");
+// 
+//  System.out.println("num=" + numrows);
+//  
+//}
   
-  
-  
-  
+//  @Disabled
   @Test
   void testThatJeepsAreReturnedWhenAValidModelAndTrimAreSupplied() {
     //given: a valid model, trim and URI
@@ -57,11 +72,41 @@ private String getBaseUri() {
     String trim = "Sport";
     String uri = String.format("%s?model=%s&trim=%s", getBaseUri(), model, trim);
 //    // When: a connection is made to the URI
-    ResponseEntity<Jeep> response = getRestTemplate().getForEntity(uri, Jeep.class);
+    ResponseEntity<List<Jeep>> response = getRestTemplate().exchange(uri, HttpMethod.GET, null, new ParameterizedTypeReference<>() {});
 //    
 //    // Then: a success (OK - 200) status code is returned
    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+   
+   //And: the actual list returned is the same as the expected list
+   List<Jeep> expected = buildExpected();
+   assertThat(response.getBody()).isEqualTo(expected);
   }
+  
+  protected List<Jeep> buildExpected() {
+    List<Jeep> list = new LinkedList<>();
+    
+    list.add(Jeep.builder()
+        .modelId(JeepModel.WRANGLER)
+        .trimLevel("Sport")
+        .numDoors(2)
+        .wheelSize(17)
+        .basePrice(new BigDecimal("28475.00"))
+        .build());
+    
+    list.add(Jeep.builder()
+        .modelId(JeepModel.WRANGLER)
+        .trimLevel("Sport")
+        .numDoors(4)
+        .wheelSize(17)
+        .basePrice(new BigDecimal("31975.00"))
+        .build());
+    
+    
+ 
+    
+    return list;
+  }
+ 
  
   }
 
